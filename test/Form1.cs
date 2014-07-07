46568c4b451f7912f1667bb31462336b4bcfe065
anthony820911 <anthony820911@gmail.com>
2014-07-07 16:18:33 +0800

    test1 commit test

--- test/Form1.cs
+++ test/Form1.cs
@@ -20,7 +20,7 @@ namespace test
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = "test";
-            MessageBox.Show("ABC");
+            MessageBox.Show("ABC2");
 
         }
     }
